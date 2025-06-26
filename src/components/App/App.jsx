0803b77d23008c1ecee7422515575c55c97820e9
theo-abhi <theo.abhi2793@gmail.com>
2025-06-26 18:31:26 +0530

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,24 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
+  function handleChange(event) {
+    setInputText(event.target.value);
+  }
+
   function addItem(event) {
     event.preventDefault();
     if (inputText.trim() === "") return;
+
     setItems((prevItems) => {
       return [...prevItems, { id: Date.now(), text: inputText }];
     });
     setInputText("");
   }
 
-  function handleChange(event) {
-    setInputText(event.target.value);
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
   }
 
   return (
@@ -34,7 +41,7 @@ function App() {
           <span>Add</span>
         </button>
       </form>
-      <TodoList items={items} />
+      <TodoList items={items} onDelete={deleteItem} />
     </div>
   );
 }
