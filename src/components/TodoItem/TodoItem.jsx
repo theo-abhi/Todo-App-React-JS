93efa6e8dd008112d74239a317e5e4b5d59cef95
theo-abhi <theo.abhi2793@gmail.com>
2025-06-26 18:43:34 +0530

    Update TodoItem.jsx

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,10 +3,16 @@ import "./TodoItem.css";
 
 function TodoItem(props) {
   return (
-    <li className="todo-item">
+    <li
+      className={`todo-item ${props.completed ? "completed" : ""}`} // Conditionally apply 'completed' class
+      onClick={() => {
+        props.onToggleComplete(props.id); // Call toggle function on click
+      }}
+    >
+      {" "}
       {props.text}
       <button
-        className="delete-button"
+        className="delete-button" // Add a class name to the button
         onClick={() => {
           props.onDelete(props.id);
         }}
