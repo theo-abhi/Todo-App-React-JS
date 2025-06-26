d52b2b468f5f5d1dd7a88ef382506ac209b84d11
theo-abhi <theo.abhi2793@gmail.com>
2025-06-26 18:23:10 +0530

    Update TodoItem.jsx

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./TodoItem.css";
 
 function TodoItem(props) {
-  return <li>{props.text}</li>;
+  return (
+    <li className="todo-item">
+      {" "}
+      {/* Add a class name to the list item */}
+      {props.text}
+      <button
+        className="delete-button" // Add a class name to the button
+        onClick={() => {
+          props.onDelete(props.id);
+        }}
+      >
+        DEL
+      </button>
+    </li>
+  );
 }
 
 export default TodoItem;
